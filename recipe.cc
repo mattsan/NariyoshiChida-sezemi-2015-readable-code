fec65940fd569cb6003b38cef3e60c55d761d578
Eiji MATSUMOTO <e.mattsan@gmail.com>
2015-06-06 23:31:32 +0900

    spec7: レシピ情報を Recipi クラスに格納するようにした
    合わせて入出力も Recipe クラスに定義するようにしました。

--- recipe.cc
+++ recipe.cc
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-typedef vector<string> recipe_data_t;
-
 int StrToInt(const string s) {
   stringstream ss;
   ss << s;
@@ -15,25 +13,55 @@ int StrToInt(const string s) {
   return result;
 }
 
-void LoadRecipe(const string filename, recipe_data_t& recipeData) {
-  ifstream file(filename.c_str());
-  string   text;
+class Recipe {
+public:
+  istream& readFrom(istream& in) {
+    string data;
+    if(getline(in, data).good()) {
+      size_t end_of_name = data.find_first_of(' ');
+      size_t begin_of_url = data.find_first_not_of(' ', end_of_name);
+      name_ = data.substr(0, end_of_name);
+      url_  = data.substr(begin_of_url);
+    }
+    return in;
+  }
 
-  while(getline(file, text).good()) {
-    ostringstream oss;
-    oss << text;
-    recipeData.push_back(oss.str());
+  ostream& writeTo(ostream& out) const {
+    return out << name_ << ' ' << url_;
+  }
+
+private:
+  string name_;
+  string url_;
+};
+
+istream& operator >> (istream& in, Recipe& recipe) {
+  return recipe.readFrom(in);
+}
+
+ostream& operator << (ostream& out, const Recipe& recipe) {
+  return recipe.writeTo(out);
+}
+
+typedef vector<Recipe> Recipes;
+
+void LoadRecipe(const string filename, Recipes& recipes) {
+  ifstream file(filename.c_str());
+  Recipe   recipe;
+
+  while(recipe.readFrom(file).good()) {
+    recipes.push_back(recipe);
   }
 }
 
-void PrintRecipe(const recipe_data_t& recipeData, const string& id_str) {
+void PrintRecipe(const Recipes& recipes, const string& id_str) {
   if(id_str.empty()) {
-    for(int i = 0; i < recipeData.size(); ++i) {
-      cout << (i + 1) << ": " << recipeData[i] << endl;
+    for(int i = 0; i < recipes.size(); ++i) {
+      cout << (i + 1) << ": " << recipes[i] << endl;
     }
   } else {
     int id_number = StrToInt(id_str);
-    cout << id_number << ": " << recipeData[id_number - 1] << endl;
+    cout << id_number << ": " << recipes[id_number - 1] << endl;
   }
 }
 
@@ -41,10 +69,10 @@ int main(int argc,char* argv[]){
   const std::string filename = argv[1];
   const std::string id_str   = (argc > 2) ? argv[2] : "";
 
-  recipe_data_t recipeData;
+  Recipes recipes;
 
-  LoadRecipe(filename, recipeData);
-  PrintRecipe(recipeData, id_str);
+  LoadRecipe(filename, recipes);
+  PrintRecipe(recipes, id_str);
 
   return 0;
 }
