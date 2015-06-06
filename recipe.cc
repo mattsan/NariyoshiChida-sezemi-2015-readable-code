dd9ff591113cf458fffc1f940420ef13c199b51a
Narishi Chida <nariyoshichidamm@gmail.com>
2015-06-06 11:28:10 +0900

    spec2: added README.txt recipe-data.txt recipe.cc

--- recipe.cc
+++ recipe.cc
@@ -2,7 +2,24 @@
 
 using namespace std;
 
-int main(){
-  cout << "オムライス" << endl;
+void Output(string filename){
+
+  fstream read_file(filename.c_str());
+  string text;
+
+  while( !( read_file >> text ).fail() ) {
+    cout << text << endl;
+  }
+
+  read_file.close();
+
+}
+
+int main(int argc,char* argv[]){
+
+  for(int i=1;i<argc;i++){
+    Output(string(argv[i]));
+  }
+
   return 0;
 }
